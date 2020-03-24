c2f1329b1de72012dab608784cdcc8095e6a0383
Nicholas Keller <nic.keller328@gmail.com>
2020-03-24 17:36:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,12 @@
 def countdown(number)
   
   while number > 0
-  puts "#{number} SECONDS!"
+  "#{number} SECONDS!"
   number -= 1
   end
   
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
   
 end
+
+def countdown_with_sleep
