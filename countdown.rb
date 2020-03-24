f08242c49e512063bc3bd95b668a0e1cf656f07b
Nicholas Keller <nic.keller328@gmail.com>
2020-03-24 17:44:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,11 +9,15 @@ def countdown(number)
   
   "HAPPY NEW YEAR!"
   
+end
+
+
 def countdown_with_sleep(number)
   
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
+  sleep(1)
   end
   
 end
